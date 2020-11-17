650a2bab47ad55a59e383e1a82d37ecac2a771b1
Ayomide Akinbo <ayakinbo@gmail.com>
2020-11-17 10:44:49 +0100

    replaced fetch with axios
    it is what it is.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const loader = document.querySelector('.myLoader');
 const jokeSection = document.querySelector('.joke-section');
 const errorSection = document.querySelector('.error');
 
+
+// DECIDED TO USE AXIOS FOR MY HTTP REQUEST JUST TO GET A FEEL OF HOW IT WORKS.
+// LOL PLUS I REALLY HATE THE EXTRA STEP OF PARSING TO JSON FIRST. I'M TOO LAZY FOR THAT PLIX DON'T STRESS ME
+
 let showLoader = () => {
     loader.classList.add('show');
 }
@@ -28,28 +32,50 @@ let createError = () => {
     p.style.color = "red";
     errorSection.appendChild(p);
     setTimeout(()=>{p.remove()}, 1000);
+   
 }
 
 let getJoke = async () => {
-    try {
-        let joke = await fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
-        let jsonJoke = await joke.json();
-        return jsonJoke
-    } catch (error) {
-        console.log(`Please Check your network `, error.message);
-        hideLoader();
-        createError()
+
+    try{
+        let { data } = await axios.get(`https://api.chucknorris.io/jokes/random?category=${select.value}`);
+        return data.value;
     }
+    catch(error){
+        console.log(error.message, 'Check your network my gee');
+        hideLoader();
+        createError();
+       
+    }
+  
+    // try {
+    //     let joke = await fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
+    //     let jsonJoke = await joke.json();
+    //     return jsonJoke
+    // } catch (error) {
+    //     console.log(`Please Check your network `, error.message);
+    //     hideLoader();
+    //     createError()
+    // }
 }
 
 async function implement(){
-    hideJoke();
-    showLoader();
-    let norrisJoke = await getJoke();
-    joke.innerText = norrisJoke.value;
-    hideLoader();
-    showJoke();
+    try {
+        hideJoke()
+        showLoader()
+        let { data } =  await axios.get(`https://api.chucknorris.io/jokes/random?category=${select.value}`);
+        joke.innerText  = data.value;
+        hideLoader();
+        showJoke(); 
+    } 
+    catch (error) {
+        console.log(error.message)
+        hideLoader();
+        createError();
+    }
 }
+
+
 
 
 btn.addEventListener('click', implement);
